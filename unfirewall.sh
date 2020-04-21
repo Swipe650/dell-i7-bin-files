26e5925fe9892c6270d1f807301ee629881e837b
Swipe <swipe650@gmail.com>
2020-04-21 20:44:16 +0100

    allowed ssh port 22

--- unfirewall.sh
+++ unfirewall.sh
@@ -5,6 +5,7 @@ sudo ufw default deny incoming
 sudo ufw default allow outgoing
 sudo ufw allow 1714:1764/udp
 sudo ufw allow 1714:1764/tcp
+sudo ufw allow 22/tcp
 sudo ufw enable
 
 if [ -e /home/swipe/.conky/fwon.png ] 
