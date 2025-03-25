d9633015d403d1a468654b5ac22cfdf2e518fba6
Swipe <swipe650@gmail.com>
2025-03-25 15:49:15 +0000

    updated to show release and artist

--- now_playing_on_rym.sh
+++ now_playing_on_rym.sh
@@ -2,6 +2,7 @@
 
 # Get the current artist using playerctl
 artist=$(playerctl --player=wiimplay metadata artist)
+album=$(playerctl metadata album)
 
 # Check if the artist is available
 if [ -z "$artist" ]; then
@@ -11,9 +12,12 @@ fi
 
 # Replace spaces with '+' for URL encoding
 artist_encoded=$(echo "$artist" | sed 's/ /+/g')
+album_encoded=$(echo "$album" | sed 's/ /+/g')
+
 
 # Open the artist page on RateYourMusic
-xdg-open "https://rateyourmusic.com/search?searchtype=a&searchterm=$artist_encoded"
+#xdg-open "https://rateyourmusic.com/search?searchtype=a&searchterm=$artist_encoded"
+xdg-open "https://rateyourmusic.com/search?searchtype=release&searchterm=$artist_encoded%20-%20$album_encoded"
 
 # Output the artist name for reference
 echo "Now playing: $artist"
