d2ed450f4450b4a5ab87b4702118956c3ae6bbc2
Swipe <swipe650@gmail.com>
2026-03-22 13:23:23 +0000

    killall conky and start gotham, conky-rtng and conkytidal-hifi

--- startconky.sh
+++ startconky.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 sleep 5
-killall -9 conky && conky -c ~/.conky/Gotham/Gotham && conky -c ~/.conky/conky-rtng
+killall -9 conky 
+sleep 1
+conky -c ~/.conky/Gotham/Gotham && conky -c ~/.conky/conky-rtng && conky -c ~/.conky/conkytidal/conkytidal-hifi
