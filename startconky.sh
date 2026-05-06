16f9b8a87f40234f831941a0a3fa4a979da8bc57
Swipe <swipe650@gmail.com>
2026-05-06 18:28:25 +0100

    fixed so it launches all three conkies correctly

--- startconky.sh
+++ startconky.sh
@@ -2,4 +2,6 @@
 sleep 5
 killall -9 conky 
 sleep 1
-conky -c ~/.conky/Gotham/Gotham && conky -c ~/.conky/conky-rtng && conky -c ~/.conky/conkytidal/conkytidal-hifi
+conky -c ~/.conky/Gotham/Gotham &
+conky -c ~/.conky/conky-rtng &
+conky -c ~/.conky/conkytidal/conkytidal-hifi &
